b7bb200ec222565ec4b90ba43dc6f184a9037941
Aighz <kbistrup@gmail.com>
2020-10-07 19:24:04 +0200

    Add reset button and change BG color in 3 steps

--- main.js
+++ main.js
@@ -13,15 +13,37 @@ const createGrid = (size) => {
   for (i = 0; i < number; i++) {
     const box = document.createElement("div");
     box.classList.add("box");
+    box.style.backgroundColor = "rgb(255, 255, 255)";
     grid.appendChild(box);
   }
   container.appendChild(grid);
   path = document.querySelectorAll(".box");
   path.forEach((div) => {
     div.addEventListener("mouseover", () => {
-      div.style.backgroundColor = "black";
+      let color = div.style.backgroundColor
+        .slice(4, div.style.backgroundColor.length - 1)
+        .split(", ");
+      if (color[0] >= 85) {
+        color[0] -= 85;
+        color[1] -= 85;
+        color[2] -= 85;
+      }
+      div.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
     });
   });
 };
 
 window.onload = createGrid(16);
+
+const button = document.querySelector("#btn");
+button.addEventListener("click", () => {
+  let changeSize = parseInt(
+    prompt("How large do you want the grid to be? (1-100)"),
+    10
+  );
+  if (changeSize > 0 && changeSize <= 100) {
+    createGrid(changeSize);
+  } else {
+    alert("Number not in given range!");
+  }
+});
